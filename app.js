0603a8bf97a7822771388be22f479232549aadd9
aleomg <alejandro.faundes1@gmail.com>
2021-10-03 17:14:56 -0300

    Se agrega proyecto finalizado de mercado liebre

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const app = express();
 const path = require('path');
 const publicPath = path.resolve(__dirname, './public/');
 
-app.listen(4000, () => console.log('Servidor 4000 funcionando'));
-
 app.use(express.static(publicPath));
 
+
+
+app.listen(process.env.PORT || 4000, () => console.log('Servidor 4000 funcionando'));
+
+
 app.get('/', (req, res) => res.sendFile (path.resolve(__dirname, './views/home.html')));
+
+
+app.get('/register', (req, res) => res.sendFile (path.resolve(__dirname, './views/register.html')));
+
+
+app.post('/register', (req, res) => console.log('Ya te registraste campeón'));
+
+
+app.get('/login', (req, res) => res.sendFile (path.resolve(__dirname, './views/login.html')));
